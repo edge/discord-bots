8d4395c5ffbd524b20315b537403dc7f0c6e5b2a
Will Garrett <willgarrett64@yahoo.com>
2022-07-13 17:03:33 +0100

    feat: add cors

--- src/network-bot/api/index.ts
+++ src/network-bot/api/index.ts
@@ -5,7 +5,20 @@
 // import * as metrics from './metrics'
 // import cors from 'cors'
 import { NetworkBot } from '..'
+import cors from 'cors'
+import { version } from '../../../package.json'
 import express, { ErrorRequestHandler, RequestHandler } from 'express'
+
+/**
+ * Index handler.
+ * Displays product name and version.
+ */
+const index: RequestHandler = (req, res) => {
+  res.json({
+    name: 'network_bot',
+    version
+  })
+}
 
 export class API {
   private app: express.Express
@@ -18,9 +31,10 @@ export class API {
 
   async initialize (): Promise<void> {
     const app = this.app
-    // app.use(cors())
+    app.use(cors())
     app.use(express.json())
 
+    app.get('/', index)
     app.get('/metrics', async (req, res) => {
       res.send(await this.network.metricsRegistry.registry.metrics())
     })
